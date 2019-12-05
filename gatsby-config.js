def4e47aa736190b50c384783974360fcb563f12
Nur Liyana Mohd Lazim <liyana@goquo.com>
2019-12-05 18:14:04 +0800

    Change site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Liyana Lazim's default portfolio page`,
+    title: `Liyana Lazim`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
