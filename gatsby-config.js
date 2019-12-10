c4e440925e92f856c412ad5b6c070d7740103d05
Nur Liyana Mohd Lazim <liyana@goquo.com>
2019-12-10 17:34:36 +0800

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#4c4c4c`,
         theme_color: `#4c4c4c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/oleander.png`, // This path is relative to the root of the site.
       },
     },
     {
