05dc0db930744013775531853ec75c8150cf83f8
Liyana Lazim <liyanalzm@gmail.com>
2019-12-27 15:00:45 +0800

    Change manifest name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Liyana Lazim's portfolio`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#4c4c4c`,
