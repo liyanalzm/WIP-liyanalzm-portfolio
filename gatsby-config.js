625cc1ec3388a7e0db31f115d7b6b3c8b31192c0
Liyana Lazim <liyanalzm@gmail.com>
2020-02-03 15:17:28 +0800

    Fix site description and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Liyana Lazim`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `All about Liyana Lazim is here. Her education, portfolio, achievements and many more.`,
+    author: `@liyanalzm`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
